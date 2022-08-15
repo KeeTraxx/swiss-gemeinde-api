a419e9abd1de90a1e5ce040c6067ebb62791cd7b
Khôi Tran <kt@compile.ch>
2022-08-15 07:27:03 +0200

    wip

--- src/svelte/Display.svelte
+++ src/svelte/Display.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
   import { afterUpdate } from 'svelte';
   import { scaleLinear, select } from 'd3';
+  import { geoPath } from 'd3';
+  import { geoMercator } from 'd3';
   export let data;
   let svg;
+  let layerBorders;
 
-  const 
+  const proj = geoMercator();
+  const drawer = geoPath().projection(proj);
 
   afterUpdate(() => {
-    console.log('afterupdate', data);
     const scale = scaleLinear();
-    console.log(scale, svg);
+    if (!data) {
+        return;
+    }
+    
+    proj.fitSize([svg.clientWidth, svg.clientHeight ], data);
+    select(layerBorders)
+      .selectAll('path')
+      .data(data.features)
+      .join((enter) => enter.append('path'));
+    redraw();
   });
+
+  function redraw() {
+    select(layerBorders)
+      .selectAll('path')
+      .attr('d', d => drawer(d));
+  }
+
 </script>
 
+<svelte:window on:resize={redraw} />
+
 <svg bind:this={svg}>
-
+  <g bind:this={layerBorders} />
 </svg>
-
